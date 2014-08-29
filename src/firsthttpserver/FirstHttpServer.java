669db8e9b00a2ec7d72775a7d558ea72031a8066
smarandadungeanu <smarandadungeanu@10.50.137.100>
2014-08-29 11:56:02 +0200

    Added page that shows Request Headers

--- src/firsthttpserver/FirstHttpServer.java
+++ src/firsthttpserver/FirstHttpServer.java
@@ -7,6 +7,9 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class FirstHttpServer {
 
@@ -21,10 +24,55 @@ public class FirstHttpServer {
         InetSocketAddress i = new InetSocketAddress("127.0.0.1", 8080);
         HttpServer server = HttpServer.create(i, 0);
         server.createContext("/welcome", new WelcomeHandler());
+        server.createContext("/headers", new HeadersHandler());
         server.setExecutor(null);
         server.start();
         System.out.println("Started the server, listening on: " + port);
         System.out.println("Bound to: " + ip);
+    }
+
+    static class HeadersHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            Map headers = he.getRequestHeaders();
+            String response;
+            Iterator entries = headers.entrySet().iterator();
+            StringBuilder sb = new StringBuilder();
+            sb.append("<!DOCTYPE html>\n");
+            sb.append("<html>\n");
+            sb.append("<head>\n");
+            sb.append("<title>My fancy Web Site</title>\n");
+            sb.append("<meta charset='UTF-8'>\n");
+            sb.append("</head>\n");
+            sb.append("<body>\n");
+            sb.append("<table border=1>\n");
+            sb.append("<tr>\n");
+            sb.append("<th>Header</th>\n");
+            sb.append("<th>Value</th>\n");
+            sb.append("</tr>\n");
+            while (entries.hasNext()) {
+                Entry thisEntry = (Entry) entries.next();
+                Object key = thisEntry.getKey();
+                Object value = thisEntry.getValue();
+                sb.append("<tr>\n");
+                sb.append("<td>" + key.toString() + "</td>\n");
+                sb.append("<td>" + value.toString() + "</td>\n");
+                sb.append("</tr>\n");
+            }
+            sb.append("</table>");
+            sb.append("</body>\n");
+            sb.append("</html>\n");
+            response = sb.toString();
+            Headers h = he.getResponseHeaders();
+            h.add("Content-Type", "text/html");
+
+            he.sendResponseHeaders(200, response.length());
+            try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
+                pw.print(response);
+            };
+        }
+
     }
 
     static class WelcomeHandler implements HttpHandler {
