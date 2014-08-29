3a25ffbdaa35f15b87bc2e9de9e4ee8391387aef
smarandadungeanu <smarandadungeanu@10.50.137.100>
2014-08-29 11:10:44 +0200

    Response is now HTML

--- src/firsthttpserver/FirstHttpServer.java
+++ src/firsthttpserver/FirstHttpServer.java
@@ -32,6 +32,21 @@ public class FirstHttpServer {
         @Override
         public void handle(HttpExchange he) throws IOException {
             String response = "Welcome to my first fantastic server! :D";
+            StringBuilder sb = new StringBuilder();
+            sb.append("<!DOCTYPE html>\n");
+            sb.append("<html>\n");
+            sb.append("<head>\n");
+            sb.append("<title>My fancy Web Site</title>\n");
+            sb.append("<meta charset='UTF-8'>\n");
+            sb.append("</head>\n");
+            sb.append("<body>\n");
+            sb.append("<h2>Welcome to my very first home made Web Server :-)</h2>\n");
+            sb.append("</body>\n");
+            sb.append("</html>\n");
+            response = sb.toString();
+            Headers h = he.getResponseHeaders();
+            h.add("Content-Type", "text/html");
+
             he.sendResponseHeaders(200, response.length());
             try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
                 pw.print(response);
